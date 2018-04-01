7b0923791ff74eb23b0cf9e0f3ac1561ec9d4078
Dikower <dmitriy1d01@gmail.com>
2018-04-01 20:10:53 +0300

    Update loading.py
    Пофиксил раздражающие прыжки герба, теперь нормально крутится, подправил код под стандарты pep8.

--- comrade - The game/loading.py
+++ comrade - The game/loading.py
@@ -16,42 +16,52 @@ def load_image(name, colorkey = None):
         image.set_colorkey(colorkey)
     return image
 
+
 pygame.init()
-size = width,height = (830,385)
+size = width, height = (830, 385)
 speed = 200
-screen = pygame.display.set_mode(size);
-screen.fill((0,0,0));
-redworld = load_image('redworld.png');
+screen = pygame.display.set_mode(size)
+screen.fill((0, 0, 0))
+redworld = load_image('redworld.png')
 
-
-image = load_image('has100.png');
-running = True;
-coords = [365, -300]
+image = load_image('has100.png')
+running = True
+rect = image.get_rect()
+coords = [width//2 - rect.w//2, 0]
+center = (width//2, height//2)
 
 image1 = image
-agle = 0 
+agle = 0
 
+
+ID_TIMER = 10
+pygame.time.set_timer(ID_TIMER, 20)
+rotating = False
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False;
-    if coords[1]<120:
-        pygame.time.wait(5)
-        coords[1] += 1
-    else:
-        pygame.time.wait(5)
-        image1 = pygame.transform.rotate(image, agle)
-        agle+=1
-        if agle>=359:
-            agle = 0
-        #image = pygame.transform.rotate(image,1)
-        #Не работает нормально 
-        #Приделал костыль
-    screen.fill((0, 0, 0));
-    screen.blit(redworld,(0,0))
-    screen.blit(image1,coords)
-    print("Loading data"+random.randint(1,3)*'.')
-   # print(coords)
-    pygame.display.flip();
-pygame.quit();
+            running = False
+        if event.type == ID_TIMER:
+            if coords[1] < (height//2 - rect.h//2) and not rotating:
+                coords[1] += 1
+
+            else:
+                rotating = True
+                image1 = pygame.transform.rotate(image, agle)
+                rect1 = image1.get_rect()
+                coords = (center[0] - rect1.width // 2, center[1] - rect1.height // 2)
+                agle += 1
+                if agle >= 359:
+                    agle = 0
+                # Не работает нормально
+                # Приделал костыль
+
+    screen.fill((0, 0, 0))
+    screen.blit(redworld, (0, 0))
+    screen.blit(image1, coords)
+
+    # print("Loading data"+random.randint(1, 3)*'.')
+    pygame.display.flip()
+
+pygame.quit()
