af183a752a9efe8a476a1b8bf21d2d114ac7a6d7
Dikower <dmitriy1d01@gmail.com>
2018-04-10 20:58:35 +0300

    Добавлены враги

--- comrade - The game/game_play.py
+++ comrade - The game/game_play.py
@@ -41,7 +41,7 @@ def write_image(response):
         sys.exit(2)
 
 
-def load_image(name, colorkey = None):
+def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
     try:
         image = pygame.image.load(fullname)
@@ -91,11 +91,11 @@ class Everything(pygame.sprite.Sprite):
 
 
 class Hero(Everything):
-    def __init__(self, *group, center, coors, surface, size, bullets, lon, lat, layer, spn):
+    def __init__(self, *group, center, coors, surface, size, bullets, all_sprites, lon, lat, layer, spn):
         self.CONST_SPEED = 2
 
         self.health = 100
-        self.health_rect = pygame.Surface([100, 5])
+        self.health_rect = pygame.Surface([200, 5])
         self.health_rect.fill((255, 0, 0))
 
         self.direction = 0
@@ -114,12 +114,13 @@ class Hero(Everything):
         write_image(self.response)
         self.location = load_image("map.png")
         self.Bullets = bullets
+        self.all_sprites = all_sprites
 
         super().__init__(*group, coors=coors, images=images, loading=True)
         self.old_rect = copy.deepcopy(self.rect)
 
     def fire(self):
-        Bullet(self.Bullets,
+        Bullet(self.Bullets, self.all_sprites, size=self.size, comand=1,
                coors=(self.rect.x + self.rect.w // 2, self.rect.y + self.rect.h // 2), direction=self.direction)
 
     def players_control(self, event):
@@ -157,10 +158,15 @@ class Hero(Everything):
                 self.pressed = False
 
     def update(self):
-        if self.health < 1:
-            return False
-        self.health_rect = pygame.Surface([self.health, 5])
-        self.health_rect.fill((255, 0, 0))
+        bullet = pygame.sprite.spritecollideany(self, self.Bullets, collided=False)
+        if bullet:
+            if bullet.comand != 1:
+                self.health -= bullet.damage
+                if self.health < 1:
+                    return False
+                self.health_rect = pygame.Surface([self.health * 2, 5])
+                self.health_rect.fill((255, 0, 0))
+                bullet.kill()
 
         if self.pressed:
             self.update_frame()
@@ -233,10 +239,14 @@ class Hero(Everything):
             # self.rect.bottom = self.rect.y + self.rect.h
             self.old_rect = copy.deepcopy(self.rect)
 
+            # TODO import scoreworking
+            # scoreworking, scoresetvalue((coord[0], coord[1]), 'current coordinats')
+            # (13.406888, 52.517694)
+
     def render(self):
         self.surface.blit(self.location, (0, 0))
         self.surface.blit(self.image, self.rect)
-        self.surface.blit(self.health_rect, (self.size[0] - 110, self.size[1] - 20))
+        self.surface.blit(self.health_rect, (self.size[0] - 210, 20))
 
 
 class Loot(Everything):
@@ -245,57 +255,84 @@ class Loot(Everything):
 
 
 class Enemy(Everything):
-    def __init__(self, *group, health, coors, hero):
-        self.CONST_SPEED = 8
+    def __init__(self, *group, health, surface, size, Bullets, all_sprites, coors, hero):
+        self.CONST_SPEED = 3
         self.hero = hero
-
+        self.surface = surface
         self.images = ['Enemies/Enemy.png']
+        self.size = size
+        self.Bullets = Bullets
+        self.all_sprites = all_sprites
         super().__init__(group, coors=coors, images=self.images, loading=True)
         self.direction = -int(math.degrees(math.atan2(self.rect.center[1] - self.hero.rect.y,
                                                       self.rect.center[0] - self.hero.rect.x))) + 90
 
         self.image = pygame.transform.rotate(self.image, self.direction)
         self.health = health
-        self.ID = 2
-        pygame.time.set_timer(self.ID, 3000)
+
+        self.old_rect = copy.deepcopy(self.rect)
+
+    def fire(self):
+        Bullet(self.Bullets, self.all_sprites, size=self.size, comand=2,
+               coors=(self.rect.x + self.rect.w // 2, self.rect.y + self.rect.h // 2), direction=self.direction)
 
     def update(self, event):
-        if event.type == self.ID:
-            pass
+        bullet = pygame.sprite.spritecollideany(self, self.Bullets, collided=False)
+        if bullet:
+            if bullet.comand != 2:
+                self.health -= bullet.damage
+                if self.health < 1:
+                    self.kill()
+                bullet.kill()
+        if event.type == 2:
+            try:
+                self.direction = -int(math.degrees(math.atan2(self.rect.center[1] - event.pos[1],
+                                                              self.rect.center[0] - event.pos[0]))) + 90
+
+                self.image = pygame.transform.rotate(self.copy_images[self.current_frame], self.direction)
+                rect1 = self.image.get_rect()
+
+                old_center = ((self.rect.x + self.rect.w // 2), (self.rect.y + self.rect.h // 2))
+                new_center = ((self.rect.x + rect1.w // 2), (self.rect.y + rect1.h // 2))
+                coors = (
+                    self.old_rect.x - (new_center[0] - old_center[0]), self.old_rect.y - (new_center[1] - old_center[1]))
+                self.rect.x = coors[0]
+                self.rect.y = coors[1]
+            except:
+                pass
+            self.fire()
 
         delta_x = -int(round(math.cos(math.radians(self.direction + 90)))) * self.CONST_SPEED
         delta_y = -int(round(math.sin(math.radians(self.direction + 90)))) * self.CONST_SPEED
         self.rect.x -= delta_x
         self.rect.y += delta_y
 
-        if event.type == pygame.MOUSEMOTION:
-            self.direction = -int(math.degrees(math.atan2(self.rect.center[1] - self.hero.rect.center[1],
-                                                          self.rect.center[0] - self.hero.rect.center[0]))) + 90
-
-            self.image = pygame.transform.rotate(self.copy_images[self.current_frame], self.direction)
-            rect1 = self.image.get_rect()
-
-            old_center = ((self.rect.x + self.rect.w // 2), (self.rect.y + self.rect.h // 2))
-            new_center = ((self.rect.x + rect1.w // 2), (self.rect.y + rect1.h // 2))
-            coors = (
-            self.old_rect.x - (new_center[0] - old_center[0]), self.old_rect.y - (new_center[1] - old_center[1]))
-            self.rect.x = coors[0]
-            self.rect.y = coors[1]
+    # def render(self):
+    #     health_rect = pygame.Surface([0.2 * self.health, 3])
+    #     health_rect.fill((255, 0, 0))
+    #     try_image = self.image.get_rect()
+    #     self.surface.blit(health_rect, [self.old_rect.x + try_image.w//2 - health_rect.get_rect().w//2, try_image.h//2 + 5])
 
 
 class Bullet(Everything):
-    def __init__(self, *group, coors, direction):
-        self.CONST_SPEED = 10
+    def __init__(self, *group, comand, size, coors, direction):
+        self.CONST_SPEED = 7
+        self.damage = random.randint(6, 15)
         self.direction = direction
         self.images = ['Weapon/Bullet.png']
-        super().__init__(group, coors=coors, images=self.images, loading=True)
+        self.comand = comand
+        super().__init__(*group, coors=coors, images=self.images, loading=True)
         self.image = pygame.transform.rotate(self.image, self.direction)
+        self.size = size
 
     def update(self):
         delta_x = -int(round(math.cos(math.radians(self.direction + 90)))) * self.CONST_SPEED
         delta_y = -int(round(math.sin(math.radians(self.direction + 90)))) * self.CONST_SPEED
         self.rect.x -= delta_x
         self.rect.y += delta_y
+
+        if (not (-30 < self.rect.x < self.size[0] + 30)) and (not (-30 < self.rect.y < self.size[1])):
+            self.kill()
 
 
 class Game_play:
@@ -308,9 +345,12 @@ class Game_play:
         self.size = size
 
         self.Bullets = pygame.sprite.Group()
-        self.Units = pygame.sprite.Group()
-        self.hero = Hero(self.all_sprites, self.Units, center=self.center, coors=(self.center[0] - 50, self.center[1] - 50),
-                         surface=surface, size=size, bullets=self.Bullets, lon=lon, lat=lat, layer=layer, spn=spn)
+        self.enemies = pygame.sprite.Group()
+        self.hero = Hero(self.all_sprites, center=self.center, coors=(self.center[0] - 50, self.center[1] - 50),
+                         surface=surface, size=size, bullets=self.Bullets, lon=lon, lat=lat, layer=layer, spn=spn,
+                         all_sprites=self.all_sprites)
+        self.SPAWN_ID = 4
+        pygame.time.set_timer(self.SPAWN_ID, 10000)
 
     def hero_updater(self):
         self.hero.update()
@@ -322,10 +362,15 @@ class Game_play:
     def event_tracker(self, event):
         self.hero.players_control(event)
 
+    def enemies_updater(self, event):
+        for enemy in self.enemies:
+            enemy.update(event)
+
     def update(self, event):
         self.threading_update = [threading.Thread(target=self.event_tracker(event)),
                                  threading.Thread(target=self.hero_updater),
-                                 threading.Thread(target=self.Bullets_update)]
+                                 threading.Thread(target=self.Bullets_update),
+                                 threading.Thread(target=self.enemies_updater(event))]
         for sprite in self.all_sprites:
             if not -50 < sprite.rect.x < self.size[0] + 50 or not -50 < sprite.rect.y < self.size[1] + 50:
                 sprite.kill()
@@ -335,13 +380,15 @@ class Game_play:
         for i in self.threading_update:
             i.join()
 
-        if len(self.Units) < 2:
-            for i in range(random.randint(1, 5)):  # def __init__(self, *group, health, coors, direction):
-                Enemy(self.Units, self.all_sprites, health=random.randint(50, 200), coors=(random.randint(0, self.size[0]),
-                                                                                           random.randint(0, self.size[1])),
-                      hero=self.hero)
+        if len(self.enemies) < 1 and event.type == self.SPAWN_ID:
+            for i in range(random.randint(0, 3)):  # def __init__(self, *group, health, coors, direction):
+                Enemy(self.enemies, self.all_sprites, size=self.size, Bullets=self.Bullets, all_sprites=self.all_sprites,
+                      health=random.randint(10, 40), surface=self.surface, coors=(random.randint(0, self.size[0]),
+                                                             random.randint(0, self.size[1])), hero=self.hero)
 
     def render(self):
+        # for unit in self.enemies:
+        #     unit.render()
         self.hero.render()
         self.all_sprites.draw(self.surface)
 
@@ -357,6 +404,10 @@ def main():
     game = Game_play(size, screen, lon, lat, layer[1], spn)
     TIMER_ID = 10
     pygame.time.set_timer(TIMER_ID, 20)
+
+    FIRE_ID = 2
+    pygame.time.set_timer(FIRE_ID, 300)
+
     running = True
     while running:
         for event in pygame.event.get():
@@ -371,3 +422,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
