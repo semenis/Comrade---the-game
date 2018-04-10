3d46dff70e7213c5957e4225a9b401167b4879de
Семён <semen.say@ya.ru>
2018-04-10 18:11:51 +0300

    Add files via upload

--- comrade - The game/combot.py
+++ comrade - The game/combot.py
@@ -2,19 +2,46 @@ from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
 from telegram import ReplyKeyboardMarkup
 import time
 import os
-
+import requests
 import config
 
 bot_token = config.telegram_bot_api_key
 
-reply_keyboard = [['/score', '/rules'], ['/quests', '/up_my_rang']]
+reply_keyboard = [['/score', '/rules'], ['/quests', '/up_my_rang'], ['/GPS']]
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
 
 quests = ['kills', 'desanting times', 'exp']
 points = [1, 5, 20, 50, 100, 500, 1000, 5000]
 
 rangs = {0: 'comrade robot', 10: 'Private', 30: 'Ensign', 50: 'Lieutenant', 100: 'Commandante', 500: 'Captain',
-         2000: 'Red Army General'}
+         2000: 'Red Army General', 5000: 'Marshall with Lenin orden'}
+
+def finder(long_lan):
+    geocoder_request = "http://geocode-maps.yandex.ru/1.x/?geocode={0}&format=json".format(long_lan)
+
+    # Выполняем запрос.
+    response = None
+    try:
+        response = requests.get(geocoder_request)
+        if response:
+            # Преобразуем ответ в json-объект
+            json_response = response.json()
+
+            # Получаем первый топоним из ответа геокодера.
+            # Согласно описанию ответа он находится по следующему пути:
+            toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+            # Полный адрес топонима:
+            #print(toponym)
+            ret = toponym['metaDataProperty']['GeocoderMetaData']['text']
+
+            #print(ret)
+            return (ret)
+        else:
+            return ("Ошибка выполнения запроса:")
+            print(geocoder_request)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+    except:
+        return ("Запрос не удалось выполнить. Проверьте наличие сети Интернет.")
 
 
 # Определяем функцию-обработчик сообщений.
@@ -45,6 +72,7 @@ def main():
     dp.add_handler(CommandHandler("rules", rules))
     dp.add_handler(CommandHandler("quests", quests_))
     dp.add_handler(CommandHandler("up_my_rang", up_my_rang))
+    dp.add_handler(CommandHandler("GPS", gps))
     # Запускаем цикл приема и обработки сообщений.
     updater.start_polling()
 
@@ -71,7 +99,7 @@ def score(bot, update):
 
 def rules(bot, update):
     update.message.reply_text(
-        "Ваша задача десантироваться на вражескую территорию и передвигаясь по карте, уничтожить врагов коммунизма, играя за гиганского робота. Вперед к победе, Comrade!")
+        "Ваша задача десантироваться на вражескую территорию и передвигаясь по карте, уничтожить врагов коммунизма, играя за гиганского робота. Вперед к победе, Comrade!\nДля управления роботом используйте стрелочки, мышь. Для стрельбы - кнопка мыши.")
 
 
 def quests_(bot, update):
@@ -107,6 +135,23 @@ def up_my_rang(bot, update):
             c.append(i)
     update.message.reply_text('Вы заслуживаете ранг: ' + rangs[max(c)])
     scoreworking.scoresetvalue(rangs[max(c)], 'rang')
+    with open('data/lvlup.mp3', 'rb') as file:
+        # print(file)
+        print(bot.send_voice(update.message.chat.id, file))
+
+
+def gps(bot, update):
+    import scoreworking
+    a = scoreworking.downloadscore()
+
+    coords = a['current coordinats']
+
+    update.message.reply_text('Последник раз вы замечены в : ' + str(coords))
+    try:
+
+        bot.send_venue(update.message.chat.id, latitude=coords[1], longitude=coords[0], title='Coords', address=finder(str(coords[0])+','+str(coords[1])))
+    except Exception as e:
+        print(e)
 
 
 # Запускаем функцию main() в случае запуска скрипта.
