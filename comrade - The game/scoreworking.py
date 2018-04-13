00ac73601d145188c29a28fd804726be0dea12e9
Dikower <dmitriy1d01@gmail.com>
2018-04-13 15:06:32 +0300

    changet names a to data

--- comrade - The game/scoreworking.py
+++ comrade - The game/scoreworking.py
@@ -1,26 +1,26 @@
 # Возвращает весь текущий score в виде словаря
 def downloadscore():
     with open('data/score') as file:
-        a = eval(file.readline())
-        return a
+        data = eval(file.readline())
+        return data
 
 
 # Изменяет на значение change заданное значение score (Внимание, только числовые значения), change так же может быть отрицателен
 def intscorechange(change=0, name='kills'):
     with open('data/score') as file:
-        a = eval(file.readline())
-    a[name] += change
+        data = eval(file.readline())
+    data[name] += change
     with open('data/score', 'w') as file:
-        file.write(str(a))
+        file.write(str(data))
 
 
 # Задает значение value заданному score. Value - любой, но следите за совместимостью.
 def scoresetvalue(value, name):
     with open('data/score') as file:
-        a = eval(file.readline())
-    a[name] = value
+        data = eval(file.readline())
+    data[name] = value
     with open('data/score', 'w') as file:
-        file.write(str(a))
+        file.write(str(data))
 
 
 # Сбрасывает score до начального состояния
